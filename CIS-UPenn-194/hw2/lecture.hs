a5be0ce54b1a6fbbacbaf5d2776ce10520d7d260
Todd Qualiano <30850741+gethbot42@users.noreply.github.com>
2022-02-25 11:32:14 -0500

    Create lecture.hs

---
+++ CIS-UPenn-194/hw2/lecture.hs
@@ -0,0 +1,9 @@
+-- notes from https://www.cis.upenn.edu/~cis194/spring13/lectures/02-ADTs.html
+
+data Thing = Shoe
+           | Ship
+		   | SealingWax
+		   | Cabbage
+		   | King
+  deriving Show
+
